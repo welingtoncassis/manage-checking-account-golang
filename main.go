f7eb7a15b77da4b9c6afac9f05ace4d9a244d6ed
welingtoncassis <welingtoncassis@gmail.com>
2021-12-17 12:20:04 -0300

    add method withdraw

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Account struct {
 	holder        string
 	numberAgency  int
@@ -9,4 +11,24 @@ type Account struct {
 
 func main() {
 
+	firstAccount := Account{"Welington", 589, 123456, 125.5}
+
+	// var firstAccount *Account
+	// firstAccount = new(Account)
+	// firstAccount.balance = 500
+
+	withdrawalValue := 200.0
+	fmt.Println(firstAccount.Withdraw(withdrawalValue))
+
+}
+
+func (c *Account) Withdraw(withdrawalValue float64) string {
+
+	isOk := withdrawalValue > 0 && withdrawalValue <= c.balance
+	if isOk {
+		c.balance -= withdrawalValue
+		return "successful withdrawal"
+	} else {
+		return "insufficient funds"
+	}
 }
